d3230b35223be23b9470fa8b7ba6e1558f87e485
Gowtham Pollam <gowthampollam@gmail.com>
2019-09-12 00:14:47 +0530

    Add files via upload
    This Is the Basic Structure File of the Project.

---
+++ SaveOnTech/Amazon/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AmazonConfig(AppConfig):
+    name = 'Amazon'
---
+++ SaveOnTech/SnapDeal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SnapdealConfig(AppConfig):
+    name = 'SnapDeal'
